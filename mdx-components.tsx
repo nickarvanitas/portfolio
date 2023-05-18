0525ef0f56eabaa81bb72d541a904e6a52c711e8
Nick Arvanitas <nick.arvanitas@gmail.com>
2023-05-18 14:11:51 -0600

    commit

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,4 @@
+import type { MDXComponents } from 'mdx/types';
 
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
@@ -5,7 +6,7 @@
 // other libraries.
 
 // This file is required to use MDX in `app` directory.
-export function useMDXComponents(components) {
+export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
