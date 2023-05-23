0bfd078a902c614e8008c0f5ec8f7eadca7228b4
Nick Arvanitas <nick.arvanitas@gmail.com>
2023-05-23 16:55:56 -0600

    commit

--- components/HeroExperience.tsx
+++ components/HeroExperience.tsx
@@ -11,6 +11,7 @@ export default function HeroExperience({ className }: { className?: string }) {
       <Canvas shadows>
         <OrthographicCamera makeDefault position={[0, 2, 100]} zoom={60} />
         <ambientLight intensity={1} />
+        <HeroExperienceObject />
       </Canvas>
     </div>
   )
--- components/HeroExperienceObject.tsx
+++ components/HeroExperienceObject.tsx
@@ -4,21 +4,18 @@ import { useFrame } from '@react-three/fiber'
 
 export default function HeroExperienceObject(props) {
   const logo = useRef()
-  const scroll = useScroll()
-  const tl = useRef()
-  const [rotation, setRotation] = useState(0)
+  const object = useRef()
 
   useFrame((state, delta) => {
-    setRotation(rotation + delta)
+    object.current.rotation.y = Math.sin(delta) * .4
   })
-
 
   return (
     <group {...props} dispose={null} ref={logo} scale={4}>
       <Float floatIntensity={2}>
         <group position={[0, 0, 0]} >
           <group rotation={[0, 0, Math.PI * .68]}>
-            <group rotation={[0, Math.sin(rotation) * .4, 0]}>
+            <group ref={object}>
               <mesh castShadow receiveShadow position={[-2.5, 0, 0]}>
                 <capsuleGeometry args={[1, 2.6, 4, 24]} />
                 <meshBasicMaterial toneMapped={false} wireframe  >
